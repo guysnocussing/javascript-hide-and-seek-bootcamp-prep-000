ca8fa515f9f2e90c460cbb08547476c39acd05ef
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-24 06:22:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].firstChild.nodeValue = Number(lis[i].firstChild.nodeValue) + n;
+  }
+}
