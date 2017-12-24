6dda4c575b381cb1629af22579863fbe903aaf67
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-24 01:40:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
-  let nodeSelect = parseInt(lis[i].innerHTML);
   for (let i = 0; i < lis.length; i++) {
+      let nodeSelect = parseInt(lis[i].innerHTML);
     nodeSelect = n;
   }
 }
