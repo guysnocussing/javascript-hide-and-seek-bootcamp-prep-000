6b7cc0a19a1f2684cd4f8df92bc5dc8c1539dd56
JONATHAN EASTIN <j.d.east@icloud.com>
2017-12-24 00:43:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+  console.log(document.querySelectorAll('.ranked-list').value);
+  return n;
+}
